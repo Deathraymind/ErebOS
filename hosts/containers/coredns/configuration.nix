57c7802eb4aa4013c2e30bb4bb63f1fdb5f2cb78
Deathraymind <deathraymind@gmail.com>
2026-08-28 22:41:24 +0900

    re

--- hosts/containers/coredns/configuration.nix
+++ hosts/containers/coredns/configuration.nix
@@ -9,7 +9,7 @@
 
   networking.hostName = "ct15";
   networking.useDHCP = false;
-  networking.interfaces.eth0.ipv4.addresses = [
+  networking.interfaces."eth0@if8".ipv4.addresses = [
     {
       address = "192.168.1.15";
       prefixLength = 24;
@@ -29,7 +29,35 @@
     ];
     hashedPassword = "$6$X6ADCAYJr36.atJY$aOzF6Drf0YEq2ac3QnFFU3bhJZNuY/hX9Fux6dcJCeiQTNBK1F3oFKqqlhpUoKVJA34gfIWs0VkcO1051jn5d0";
   };
+  services.coredns = {
+    enable = true;
+    config = ''
+      home.arpa {
+        hosts {
+          192.168.1.53  dns.home.arpa
+          192.168.1.10  nas.home.arpa
+          192.168.1.30  grafana.home.arpa
+          ttl 60
+          fallthrough
+        }
+      }
 
+      # --- Incus auto-records: flip this on later, once instances live on an
+      # --- Incus-owned network and you've set core.dns_address + the zone peer
+      # home.incus {
+      #   secondary {
+      #     transfer from 192.168.1.5:1053
+      #   }
+      # }
+
+      . {
+        forward . 1.1.1.1 9.9.9.9
+        cache 30
+        log
+        errors
+      }
+    '';
+  };
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  system.stateVersion = "24.05";
+  system.stateVersion = "26.05";
 }
